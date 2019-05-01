00685264da2af3e254b38fbe05ce09598164ae1c
Raul Gomez <raul_nouni@hotmail.com>
2019-05-01 18:27:25 -0500

    Numeros

---
+++ numeros.js
@@ -0,0 +1,20 @@
+var edad = 27
+
+edad += 1
+
+var peso = 75
+
+peso -= 2
+
+var sandwich = 1
+
+peso += sandwich
+
+var jugar = 3
+
+peso -= jugar
+
+var pizza = 8
+var personas = 2
+
+var porcionesPersona = pizza / personas
